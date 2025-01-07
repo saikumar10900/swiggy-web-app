a057e796787e41b81d147b175bce193ae77260d5
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2025-01-07 14:30:15 +0530

    episode 3 of namaste react

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-const root = document.getElementById("root");
-const h1 = React.createElement("h1", { id: "heading" }, "Welcome to React");
-const reactRoot = ReactDOM.createRoot(root);
-const div = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am a H1"),
-  ]),
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am a H1"),
-  ]),
-]);
-reactRoot.render(div);
+import React from "react";
+import ReactDOM from "react-dom/client";
+const h = <h1>Swiggy Web App</h1>;
+const Header = () => <h2>Delivers on time</h2>;
+const Description = () => <p>Best food delivery application you can find</p>;
+const BannerSection = () => {
+  return (
+    <div>
+      {h}
+      <Header />
+      <Description />
+    </div>
+  );
+};
+const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
+reactRoot.render(<BannerSection />);
